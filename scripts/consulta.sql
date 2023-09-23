18dd3d76b386e6151e5c04dcfbc7a4999cc9f208
Hector Bohorquez <hectorbohorqueza@gmail.com>
2023-09-23 17:40:01 +0000

    Consultas ADW

--- scripts/consulta.sql
+++ scripts/consulta.sql
@@ -1,7 +1,27 @@
 use adw;
+-- NOMBRE CLIENTE
+-- SALKES PERSON ID 
+-- NOMBRE DEL TERRITORIO
 
--- Empleados
-select *
-from HumanResources_Employee
-limit 5
-;
+SELECT 
+    sales.SalesOrderID,
+    sales.OrderDate,
+    sales.CustomerID,
+    CONCAT(person.FirstName,' ', person.LastName) AS CustomerName,
+    sales.SalesPersonID,
+    CONCAT(employee.FirstName,' ', employee.LastName) AS SalesPersonName,
+    sales.TerritoryID, 
+    salesterritory.Name AS Territory
+FROM Sales_SalesOrderHeader AS sales
+    JOIN Sales_Customer AS client
+        ON sales.CustomerID = client.CustomerID
+    LEFT OUTER JOIN Person_Person AS person
+        ON client.PersonID = person.BusinessEntityID
+    JOIN Sales_SalesPerson AS salesperson
+        ON sales.SalesPersonID = salesperson.BusinessEntityID
+    LEFT OUTER JOIN Person_Person AS employee
+        ON salesperson.BusinessEntityID = employee.BusinessEntityID
+    JOIN Sales_SalesTerritory AS salesterritory
+        ON salesterritory.TerritoryID = sales.TerritoryID
+LIMIT 10;
+    
